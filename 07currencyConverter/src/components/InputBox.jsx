65c4ee807101f97b4526420e9a6931fab2568216
bhoirprathmesh2004@gmail.com <bhoirprathmesh2004@gmail.com>
2024-08-09 19:51:39 +0530

    reactjs

--- 07currencyConverter/src/components/InputBox.jsx
+++ 07currencyConverter/src/components/InputBox.jsx
@@ -1,10 +1,10 @@
-import React, {useId} from "react";
+import React, {useId} from 'react'
 
 function InputBox({
     label,
     amount,
-    onAmountChanged,
-    onCurrencyChanged,
+    onAmountChange,
+    onCurrencyChange,
     currencyOptions = [],
     selectCurrency = "usd",
     amountDisable = false,
@@ -16,7 +16,7 @@ function InputBox({
     return (
         <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
@@ -26,7 +26,7 @@ function InputBox({
                     placeholder="Amount"
                     disabled={amountDisable}
                     value={amount}
-                    onChange={(e) => onAmountChanged && onAmountChanged(Number(e.target.value))}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
@@ -34,13 +34,13 @@ function InputBox({
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
                     value={selectCurrency}
-                    onChange={(e) => onCurrencyChanged && onCurrencyChanged(e.target.value)}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                 >
                     
                         {currencyOptions.map((currency) => (
                             <option key={currency} value={currency}>
-                                {currency}
+                            {currency}
                             </option>
                         ))}
                 
