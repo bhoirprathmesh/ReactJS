835a106dd60dbac3778cf71ffb01b8f3a06cc3c6
bhoirprathmesh2004@gmail.com <bhoirprathmesh2004@gmail.com>
2024-08-10 19:46:00 +0530

    reactjs

---
+++ 09miniContext/src/context/UserContext.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const UserContext = React.createContext()
+// context gives us Provider through which we can access the component globally.
+
+export default UserContext
