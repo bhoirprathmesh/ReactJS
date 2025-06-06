128b91f1b8cd0c742deb6d1a14a35b1d6c94b584
bhoirprathmesh2004@gmail.com <bhoirprathmesh2004@gmail.com>
2025-06-06 16:17:40 +0530

    reactjs

--- 12reduxToolkitTodo/src/features/todo/todoSlice.js
+++ 12reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export const todoSlice = createSlice({
     name: 'todo',
-    initialState,
+    // initialState,
     reducers: {
         addTodo: (state, action) => {
             const todo = {
